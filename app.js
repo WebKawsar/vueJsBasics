e4f02f922b0a28fbef53e835e2166c6e20d468be
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2020-12-23 20:41:38 +0600

    Done v-text & v-html

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 new Vue({
   el: "#app",
   data: {
-    src: "https://d30fl32nd2baj9.cloudfront.net/media/2020/01/02/technology-smart-device-010120-01.jpg/BINARY/technology-smart-device-010120-01.jpg",
-    link: "https://web.facebook.com/?_rdc=1&_rdr"
+
+    myText: "Hello Luffy",
+    code: "<strong>Hello Boss</strong>",
+
+
+
   }
  
 
