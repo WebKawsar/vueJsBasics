75f7361e0486dfaf25f2b7a08e0af2a5b6295cc7
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2020-12-24 21:08:52 +0600

    Vue js lifecycle

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
-Vue.component("Kawsar", {
-  data(){
-    return {
-      name: "Boossss"
+new Vue({
+  el: "#app",
+  data: {
+    name: "Kawsar"
+  },
+  methods: {
+    updateName() {
+      this.name = "Ahmed"
+    },
+    destroy() {
+      this.$destroy();
     }
   },
-  template: `<p>{{name}}</p>`
-})
+  beforeCreate() {
+    console.log("Before created")
+  },
+  created() {
+    console.log("Created");
+  },
+  beforeMount() {
+    console.log("Before mounted")
+  },
+  mounted() {
+    console.log("Mounted")
+  },
+  beforeUpdate() {
+    console.log("Before update")
+  },
+  updated() {
+    console.log("Updated")
+  },
+  beforeDestroy() {
+    console.log("Before destroy")
+  },
+  destroyed() {
+    console.log("Destroyed")
+  },
 
-
-new Vue({
-  el: "#app"
 })
